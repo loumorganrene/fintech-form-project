af8e2b4af6c01826d554c93c41f80fca9ea9681d
Lou René <75613368+loumorganrene@users.noreply.github.com>
2023-11-20 10:50:37 +0100

    :lipstick: Setup root styling

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ function App() {
 
   return (
     <>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam quos reiciendis doloribus inventore maxime ipsa! Aliquid modi corporis distinctio eum unde quam laborum officia nobis iste? Nam exercitationem pariatur tempora!
+    Voluptas eius nulla aut quis vero eaque consequuntur ad inventore quas porro vel repellendus libero, quos magnam aliquid nemo at vitae ea. Dolorum assumenda repudiandae numquam. Quo labore harum magnam!</p>
     </>
   )
 }
