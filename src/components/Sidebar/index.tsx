1d37304357a0281b2aa96a9635b922402cce8abe
Lou René <75613368+loumorganrene@users.noreply.github.com>
2023-11-27 18:10:34 +0100

    :construction::lipstick: Sidebar: Progression bar styling: active & passed class styling before logic

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,30 +1,36 @@
 import './Sidebar.scss'
 
 function Sidebar() {
+
+  console.log(document.querySelector(".active .progression-bar__step"))
   return (
     <aside>
       <section className="progression-bar">
         <h2 className="sidebar__title">
           Progression 1/5
         </h2>
+
         <ul className='progression-bar__list'>
 
-          <li className="progression-bar__item">
+          <li className="progression-bar__item passed">
             <span className="progression-bar__dot"></span>
+            <span className='progression-bar__step'></span>
             <a className="progression-bar__label" href="#" aria-label="Provide company's details">
               Company details
             </a>
           </li>
 
-          <li className="progression-bar__item">
+          <li className="progression-bar__item passed">
             <span className="progression-bar__dot"></span>
+            <span className='progression-bar__step'></span>
             <a className="progression-bar__label" href="#" aria-label="Present company's sources">
               Company sources
             </a>
           </li>
 
-          <li className="progression-bar__item">
+          <li className="progression-bar__item active">
             <span className="progression-bar__dot"></span>
+            <span className='progression-bar__step'></span>
             <a className="progression-bar__label" href="#" aria-label="Register company's representatives">
               Company representatives
             </a>
@@ -32,6 +38,7 @@ function Sidebar() {
 
           <li className="progression-bar__item">
             <span className="progression-bar__dot"></span>
+            <span className='progression-bar__step'></span>
             <a className="progression-bar__label" href="#" aria-label="Upload mandatory company's documents">
               Documents
             </a>
@@ -39,6 +46,7 @@ function Sidebar() {
 
           <li className="progression-bar__item">
             <span className="progression-bar__dot"></span>
+            <span className='progression-bar__step'></span>
             <a className="progression-bar__label" href="#" aria-label="Submit company's application form for review">
               Register
             </a>
