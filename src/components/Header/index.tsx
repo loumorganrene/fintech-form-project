bdf07ab191de07bcbea1ad0ba8076ead04bc2642
Lou René <75613368+loumorganrene@users.noreply.github.com>
2023-11-28 15:54:34 +0100

    :lipstick: Header: logo adjustements for responsivness

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,11 +1,20 @@
 import './Header.scss'
 import Logo from './../../assets/images/logo.svg'
+import LogoSm from './../../assets/images/logo-sm.svg'
 import Nav from '../Nav'
 
 function Header() {
   return (
     <header>
-      <img className='header-logo' src={Logo} alt="Company's logo" />
+      <picture>
+        <source media="(max-width: 768px)" srcSet={LogoSm} />
+        <source media="(min-width: 769px)" srcSet={Logo} />
+        <img
+          className='header-logo'
+          src={Logo} 
+          alt="Company's logo"
+        />
+      </picture>
       <Nav />
     </header>
   )
--- src/components/Nav/index.tsx
+++ src/components/Nav/index.tsx
@@ -2,23 +2,25 @@ import './Nav.scss'
 
 function Nav() {
   return (
-<ul className="header-nav">
-    <li>
-        <a id="user-icon" href="#" aria-label='Go to your account settings'>
-            <i className="fa-solid fa-circle-user"></i>
-        </a>
-    </li>
-    <li>
-        <a id="help-icon" href="#" aria-label='Ask for assistance'>
-            <i className="fa-regular fa-circle-question"></i>
-        </a>
-    </li>
-    <li>
-        <a id="logout-icon" href="#" aria-label='Log out'>
-            <i className="fa-solid fa-power-off"></i>
-        </a>
-    </li>
-</ul>
+<nav className="header-nav">
+    <ul className="header-nav__link">
+        <li>
+            <a id="user-icon" href="#" aria-label='Go to your account settings'>
+                <i className="fa-solid fa-circle-user"></i>
+            </a>
+        </li>
+        <li>
+            <a id="help-icon" href="#" aria-label='Ask for assistance'>
+                <i className="fa-regular fa-circle-question"></i>
+            </a>
+        </li>
+        <li>
+            <a id="logout-icon" href="#" aria-label='Log out'>
+                <i className="fa-solid fa-power-off"></i>
+            </a>
+        </li>
+    </ul>
+</nav>
   )
 }
 
